728dce3abd131d93c3c0b8532e020e2ac4cb3a6c
dontknowwhatimdoin <herburt2@hotmail.com>
2025-03-02 13:03:19 +0000

    Add files via upload

---
+++ Contents of knack-loader.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="2575.4">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; -webkit-text-stroke: #000000}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">&lt;script type="text/javascript"&gt;app_id="57e90ade0079b5533631b9b3";distribution_key="dist_17";&lt;/script&gt;&lt;script type="text/javascript" src="https://loader.knack.com/57e90ade0079b5533631b9b3/dist_17/knack.js"&gt;&lt;/script&gt;&lt;div id="knack-dist_17"&gt;Loading...&lt;/div&gt;</span></p>
+</body>
+</html>
